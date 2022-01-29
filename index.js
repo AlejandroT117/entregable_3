0ee25ee6729266b394261e0ef41d6ffe7da20506
AlejandroT117 <ualejandrotrejo@gmail.com>
2022-01-29 09:10:51 -0600

    finished

--- index.js
+++ index.js
@@ -12,11 +12,30 @@ const productoNuevo = {
   'thumbnail':'mercadolibre.com.mx'
 }
 
+app.get('/', (req, res)=>{
+  res.send('<h1 style="color: blue">Bienvenidos a la app de Alejandro T<h1/>')
+})
 
-app.get('/', (req,res)=>{
-  const objetos = productos.getAll().then((obj)=>{return obj})
+app.get('/productos', (req,res)=>{
+  productos.getAll().then((obj)=>{
+    const string_obj = JSON.stringify(obj)
 
-  res.send(`Hora actual: ${moment().format('DD/MM/YYYY HH:mm:ss')}\n Lista de productos actual: ${objetos}`)
+    res.send(`Hora actual: ${moment().format('DD/MM/YYYY HH:mm:ss')}\n Lista de productos actual: ${string_obj}`)
+  
+  })
+
+})
+
+
+app.get('/productoRandom', (req, res)=>{
+  productos.getAll().then((obj)=>{
+
+    const random = Math.floor(Math.random()*obj.length)
+    const string_obj = JSON.stringify(obj[random])
+
+    res.send(`Producto random: ${string_obj}`)
+  
+  })
 })
 
 
